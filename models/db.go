b8f7f249e066839b7293835f694cfe31b254e684
mtso <mtso@tuta.io>
2016-12-17 14:25:15 -0800

    Add log table existence check

--- models/db.go
+++ models/db.go
@@ -5,6 +5,20 @@ import (
 	_ "github.com/lib/pq"
 )
 
+const (
+	queryTableCreate = `CREATE TABLE log (
+		log_id bigserial PRIMARY KEY,
+		log_uri text NOT NULL,
+		log_description text,
+		log_timestamp timestamp WITHOUT TIME ZONE NOT NULL DEFAULT NOW()
+	);`
+	queryExists = `SELECT EXISTS ( 
+		SELECT 1
+		FROM information_schema.tables 
+		WHERE table_name='log'
+	);`
+)
+
 func InitDB(databaseType, databaseUrl string) (*sql.DB, error) {
 	db, err:= sql.Open(databaseType, databaseUrl)
 	if err != nil {
@@ -13,5 +27,11 @@ func InitDB(databaseType, databaseUrl string) (*sql.DB, error) {
 	if err = db.Ping(); err != nil {
 		return nil, err
 	}
+
+	// Create log table if it doesn't exist
+	if _, error := db.Query(queryExists); error != nil {
+		db.Exec(queryTableCreate)
+	}
+
 	return db, nil
 }
