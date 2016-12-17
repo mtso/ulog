568eda034ef0fab4e6d24f831a1396a9178264bb
mtso <mtso@tuta.io>
2016-12-16 23:45:11 -0800

    Finish up GET all logs

--- main.go
+++ main.go
@@ -3,8 +3,39 @@ package main
 import (
 	"ulog/models"
 	"database/sql"
+	"log"
+	"net/http"
+)
+
+const (
+	db_source = "user=kingcandy password=cupcakes dbname=urilog sslmode=disable"
 )
 
 type Env struct {
 	db *sql.DB
+}
+
+func main() {
+	psql, err := models.InitDB(db_source)
+	if err != nil {
+		log.Panic(err)
+	}
+	env := &Env{db: psql}
+
+	http.HandleFunc("/log", )
+}
+
+func (env *Env) retrieveLogs(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		http.Error(w, http.StatusText(405), 405)
+		return
+	}
+	logs, error := models.AllLogs(env.db)
+	if err != nil {
+		http.Err(w, http.StatusText(500), 500)
+		return
+	}
+	for _, log := range logs {
+		fmt.Fprintf(w, "%s %s: %s; \"%s...\"", log.Id, log.Timestamp, log.Description, log.Uri[:15])
+	}
 }
--- models/urilog.go
+++ models/urilog.go
@@ -6,8 +6,30 @@ import (
 )
 
 type UriLog struct {
-	Time Date
+	Timestamp Time
 	Uri string
-	Description string
-	Tags []string
+	Description sql.NullString
+	Id int
+}
+
+func AllLogs(db *sql.DB) ([]*Log, error) {
+	rows, err := db.Query("SELECT * FROM log")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	logs := make([]*Log, 0)
+	for rows.Next() {
+		book := new(Book)
+		err := rows.Scan(&log.Id, &log.Uri, &log.Description, &log.Timestamp)
+		if err != nil {
+			return nil, err
+		}
+		logs = append(logs, log)
+	}
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+	return books, nil
 }
